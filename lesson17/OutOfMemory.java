e544a11db90e26de759c63c283599b5d66fded36
Elshad00 <elsadsfqqtov@gmail.com>
2025-04-05 22:20:31 +0400

    Update OutOfMemory.java

--- lesson17/OutOfMemory.java
+++ lesson17/OutOfMemory.java
@@ -3,7 +3,6 @@ public class OutOfMemory
     public static void main(String[] args)
     {
         try {
-            // Attempt to allocate a very large array
             int[] hugeArray = new int[Integer.MAX_VALUE];
         } catch (OutOfMemoryError e) {
             System.err.println("Out of Memory Error occurred!");
